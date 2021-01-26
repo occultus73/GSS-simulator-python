7d2b11f17e9b609d3475b07f52314ceb70876736
John Rayner-Hilles <john.raynerhilles@mocha.global>
2021-01-26 01:25:17 +0000

    Created basic setup in Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+
 from business.FertileSimulator import FertileSimulator
 from business.SerileSimulator import SterileSimulator
 from data.ActualFertilityDataLoader import ActualFertilityDataLoader
@@ -8,8 +10,10 @@ from data.SterileFertilityDataLoader import SterileFertilityDataLoader
 
 TRAIT_TO_STUDY = "CONSERVATIVE"
 MIN_YEAR_OF_BIRTH = 1965
-STARTING_YEAR = 1983
+STARTING_YEAR = MIN_YEAR_OF_BIRTH + 18
 DOOMSDAY = datetime.now().year + 50
+
+years = range(STARTING_YEAR, DOOMSDAY)
 
 if __name__ == '__main__':
     # Load samples
@@ -22,9 +26,25 @@ if __name__ == '__main__':
     ideal_fertility_simulator = FertileSimulator(STARTING_YEAR, ideal_fertility_sample)
     sterile_fertility_simulator = SterileSimulator(STARTING_YEAR, sterile_fertility_sample)
 
-    # Loop through results
-    for year in range(STARTING_YEAR, DOOMSDAY):
-        print("YEAR: ", year)
-        print(actual_fertility_simulator.next_year().__len__())
-        print(ideal_fertility_simulator.next_year().__len__())
-        print(sterile_fertility_simulator.next_year().__len__())
+    # Generate the results
+    actual_fertility_results = [actual_fertility_simulator.next_year() for year in years]
+    ideal_fertility_results = [ideal_fertility_simulator.next_year() for year in years]
+    sterile_fertility_results = [sterile_fertility_simulator.next_year() for year in years]
+
+    # Analyse the data
+    actual_fertility_population_size = [pop_year.__len__() for pop_year in actual_fertility_results]
+    ideal_fertility_population_size = [pop_year.__len__() for pop_year in ideal_fertility_results]
+    sterile_fertility_population_size = [pop_year.__len__() for pop_year in sterile_fertility_results]
+
+    # Plot the data
+    plt.plot(years, actual_fertility_population_size, label="Actual")
+    plt.plot(years, ideal_fertility_population_size, label="Ideal")
+    plt.plot(years, sterile_fertility_population_size, label="Sterile")
+    plt.xlabel("Years")
+    plt.ylabel("Number Of Americans")
+    plt.title("Simulation Population Growth")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("output/Simulation_Populations.png")
+    plt.close()
