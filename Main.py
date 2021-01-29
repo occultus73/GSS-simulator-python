75494d5420494455a62db4c994e6cf3cec49a61b
John Rayner-Hilles <john.raynerhilles@mocha.global>
2021-01-29 23:08:57 +0000

    Written loop to cycle through all columns between IQ and World_Affairs inclusive.

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from data_loading.ActualFertilityDataLoader import ActualFertilityDataLoader
+from data_loading.GSS_data_columns import gss_columns
 from data_loading.IdealFertilityDataLoader import IdealFertilityDataLoader
 from data_loading.SterileFertilityDataLoader import SterileFertilityDataLoader
 from information import InformationProcessor
@@ -8,18 +9,19 @@ from meta_information import graph_sample_meta_information
 from simulation.FertileSimulator import FertileSimulator
 from simulation.SerileSimulator import SterileSimulator
 
-TRAIT_TO_STUDY = "CONSERVATIVE"
+
 MIN_YEAR_OF_BIRTH = 1965
 STARTING_YEAR = MIN_YEAR_OF_BIRTH + 18
 DOOMSDAY = datetime.now().year + 50
 
 years = range(STARTING_YEAR, DOOMSDAY)
 
-if __name__ == '__main__':
+
+def run_simulation(trait_to_study):
     # Load samples
-    actual_fertility_sample = ActualFertilityDataLoader(TRAIT_TO_STUDY, MIN_YEAR_OF_BIRTH).load_sample()
-    ideal_fertility_sample = IdealFertilityDataLoader(TRAIT_TO_STUDY, MIN_YEAR_OF_BIRTH).load_sample()
-    sterile_fertility_sample = SterileFertilityDataLoader(TRAIT_TO_STUDY, MIN_YEAR_OF_BIRTH).load_sample()
+    actual_fertility_sample = ActualFertilityDataLoader(trait_to_study, MIN_YEAR_OF_BIRTH).load_sample()
+    ideal_fertility_sample = IdealFertilityDataLoader(trait_to_study, MIN_YEAR_OF_BIRTH).load_sample()
+    sterile_fertility_sample = SterileFertilityDataLoader(trait_to_study, MIN_YEAR_OF_BIRTH).load_sample()
 
     # Setup simulators
     actual_fertility_simulator = FertileSimulator(STARTING_YEAR, actual_fertility_sample)
@@ -32,16 +34,22 @@ if __name__ == '__main__':
     sterile_fertility_results = [(year, sterile_fertility_simulator.next_year()) for year in years]
 
     # Analyse the results
-    max_trait_value = int(max([a.trait for a in sterile_fertility_sample]))
-    min_trait_value = int(min([a.trait for a in sterile_fertility_sample]))
+    max_trait_value = round(max([a.trait for a in sterile_fertility_sample]))
+    min_trait_value = round(min([a.trait for a in sterile_fertility_sample]))
 
-    InformationProcessor(TRAIT_TO_STUDY, max_trait_value, min_trait_value,
-                         f"output/{MIN_YEAR_OF_BIRTH}/{TRAIT_TO_STUDY}",
+    InformationProcessor(trait_to_study, max_trait_value, min_trait_value,
+                         f"output/{MIN_YEAR_OF_BIRTH}/{trait_to_study}",
                          actual_fertility_results,
                          ideal_fertility_results,
                          sterile_fertility_results).process_results()
 
-    graph_sample_meta_information(f"output/{MIN_YEAR_OF_BIRTH}/{TRAIT_TO_STUDY}/sample_meta_information",
+    graph_sample_meta_information(f"output/{MIN_YEAR_OF_BIRTH}/{trait_to_study}/_Sample_Meta_Information",
                                   actual_fertility_results,
                                   ideal_fertility_results,
                                   sterile_fertility_results)
+
+
+if __name__ == '__main__':
+    eligible_columns = [key for key, value in gss_columns.items() if 12 < value < 145]
+    for eligible_column in eligible_columns:
+        run_simulation(eligible_column)
